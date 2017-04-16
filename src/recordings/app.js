e51dcf33bb6f7ae0cc4e97633398b466667176c3
Oper <vbarkasov@yandex.ru>
2017-04-16 16:18:40 +0700

    Minimize app.js (remove redundant code and convert tabs to whitespaces)

--- src/recordings/app.js
+++ src/recordings/app.js
@@ -1,142 +1,116 @@
 define(function(require) {
-	var $ = require('jquery'),
-		_ = require('underscore'),
-		monster = require('monster'),
-		AWS = require('aws-sdk');
+    var $ = require('jquery'),
+        _ = require('underscore'),
+        monster = require('monster'),
+        AWS = require('aws-sdk');
 
-	var app = {
-		name: 'recordings',
+    var app = {
+        name: 'recordings',
 
-		css: [ 'app' ],
+        css: [ 'app' ],
 
-		settings: {
-			'appId': 'YOUR_APP_ID',
-			'roleArn': 'YOUR_ROLE_ARN',
-			'key': 'AKIAJR52NDF2XDN3ZUFQ',
-			'secret': '2hZCLuSz3RQK1jY002wxRknWSQY/WJJdpTILn5m5',
-			'bucketName': 'callrecordingtestforcanddi',
-			'bucketRegion': 'eu-west-2',
-			'version': 'latest'
-		},
+        settings: {
+            'appId': 'YOUR_APP_ID',
+            'roleArn': 'YOUR_ROLE_ARN',
+            'key': 'AKIAJR52NDF2XDN3ZUFQ',
+            'secret': '2hZCLuSz3RQK1jY002wxRknWSQY/WJJdpTILn5m5',
+            'bucketName': 'callrecordingtestforcanddi',
+            'bucketRegion': 'eu-west-2',
+            'version': 'latest'
+        },
 
-		i18n: {
-			'en-US': { customCss: false },
-			'fr-FR': { customCss: false }
-		},
+        i18n: {
+            'en-US': { customCss: false },
+            'fr-FR': { customCss: false }
+        },
 
-		// Defines API requests not included in the SDK
-		requests: {},
+        // Defines API requests not included in the SDK
+        requests: {},
 
-		// Define the events available for other apps
-		subscribe: {},
+        // Define the events available for other apps
+        subscribe: {},
 
-		// Method used by the Monster-UI Framework, shouldn't be touched unless you're doing some advanced kind of stuff!
-		load: function(callback) {
-			var self = this;
+        // Method used by the Monster-UI Framework, shouldn't be touched unless you're doing some advanced kind of stuff!
+        load: function(callback) {
+            var self = this;
 
-			self.initApp(function() {
-				callback && callback(self);
-			});
-		},
+            self.initApp(function() {
+                callback && callback(self);
+            });
+        },
 
-		// Method used by the Monster-UI Framework, shouldn't be touched unless you're doing some advanced kind of stuff!
-		initApp: function(callback) {
-			var self = this;
+        // Method used by the Monster-UI Framework, shouldn't be touched unless you're doing some advanced kind of stuff!
+        initApp: function(callback) {
+            var self = this;
 
-			// Used to init the auth token and account id of this app
-			monster.pub('auth.initApp', {
-				app: self,
-				callback: callback
-			});
-		},
-
-		// Entry Point of the app
-		render: function(container) {
-			var self = this,
-				parent = _.isEmpty(container) ? $('#monster-content') : container;
-
-			monster.ui.generateAppLayout(self, {
-				menus: [
-					{
-						tabs: [
-							{
-								callback: self.renderIndex
-							}
-						]
-					}
-				]
-			});
-		},
-
-		renderIndex: function(pArgs) {
-			var self = this,
-				args = pArgs || {},
-				parent = args.container || $('#recordings_app_container .app-content-wrapper'),
-				template = $(monster.template(self, 'layout', { user: monster.apps.auth.currentUser }));
-
-			parent
-				.fadeOut(function() {
-					$(this)
-						.empty()
-						.append(template)
-						.fadeIn();
-				});
-
-			AWS.config.update({
-				region: self.settings.bucketRegion,
-				version: self.settings.version,
-				credentials: {
-					'key': self.settings.key,
-					'secret': self.settings.secret
-
-				}
-			});
-
-			var s3 = new AWS.S3({
-				//apiVersion: '2006-03-01'
-			});
-
-			var s3bucket = new AWS.S3({
-				params: {
-					Bucket: 'myBucket'
-				}, apiVersion: '2006-03-01'
-			});
+            // Used to init the auth token and account id of this app
+            monster.pub('auth.initApp', {
+                app: self,
+                callback: callback
+            });
+        },
 
 
-			AWS.config.region = self.settings.bucketRegion;
-			// AWS.config.update({region: 'us-east-1'});
+        // Entry Point of the app
+        render: function(container) {
+            console.log('render');
+            var self = this,
+                parent = _.isEmpty(container) ? $('#monster-content') : container;
 
-			var bucket = new AWS.S3({
-				params: {
-					Bucket: self.settings.bucketName
-				}
-			});
-		},
-		_renderRecordingsList: function(){
-			bucket.listObjects({
-				Prefix: prefix
-			}, function (err, data) {
-				if (err) {
-					results.innerHTML = 'ERROR: ' + err;
-				} else {
-					var objKeys = "";
-					data.Contents.forEach(function (obj) {
-						objKeys += obj.Key + "<br>";
-					});
-					results.innerHTML = objKeys;
-				}
-			});
-		},
-		_putObjects: function() {
-			bucket.putObject(params, function (err, data) {
-			 if (err) {
-			 results.innerHTML = 'ERROR: ' + err;
-			 } else {
-			 listObjs();
-			 }
-			 });
-		}
-	};
+            monster.ui.generateAppLayout(self, {
+                menus: [
+                    {
+                        tabs: [
+                            {
+                                callback: self.renderIndex
+                            }
+                        ]
+                    }
+                ]
+            });
+        },
 
-	return app;
+        renderIndex: function(pArgs) {
+            var self = this,
+                args = pArgs || {},
+                parent = args.container || $('#recordings_app_container .app-content-wrapper'),
+                template = $(monster.template(self, 'layout', { user: monster.apps.auth.currentUser }));
+
+            parent
+                .fadeOut(function() {
+                    $(this)
+                        .empty()
+                        .append(template)
+                        .fadeIn();
+                });
+
+
+        },
+        _renderRecordingsList: function(){
+            bucket.listObjects({
+                Prefix: prefix
+            }, function (err, data) {
+                if (err) {
+                    results.innerHTML = 'ERROR: ' + err;
+                } else {
+                    var objKeys = "";
+                    data.Contents.forEach(function (obj) {
+                        objKeys += obj.Key + "<br>";
+                    });
+                    results.innerHTML = objKeys;
+                }
+            });
+        },
+        _putObjects: function() {
+            bucket.putObject(params, function (err, data) {
+             if (err) {
+                 results.innerHTML = 'ERROR: ' + err;
+             } else {
+                listObjs();
+             }
+             });
+        }
+    };
+
+    return app;
 });
