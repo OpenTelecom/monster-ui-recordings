2d9b53e0a185ed6becefcd565d06e218056b53b5
Oper <vbarkasov@yandex.ru>
2017-04-27 13:40:53 +0700

    Recordings Installer Builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const OUTPUT_DIR = 'recordings-installer';
-const FILE_NAME = 'src.zip';
+const INSTALLER_SRC_DIR = 'recordings-installer';
+const PROJECT_FILES_ZIP_NAME = 'src.zip';
+const INSTALLER_ZIP_NAME = 'recordings-installer.zip';
+const OUTPUT_DIR = 'dist';
 
 var gulp = require('gulp'),
 	zip = require('gulp-zip'),
@@ -9,10 +11,16 @@ var gulp = require('gulp'),
 	modify = require('gulp-modify');
 
 
-gulp.task('default', ['zip_files']);
+gulp.task('default', ['zip_project_files', 'zip_installer_files']);
 
-gulp.task('zip_files', function() {
+gulp.task('zip_project_files', function() {
 	gulp.src('src/**', { base: '.' })
-		.pipe(zip(FILE_NAME))
+		.pipe(zip(PROJECT_FILES_ZIP_NAME))
+		.pipe(gulp.dest(INSTALLER_SRC_DIR));
+});
+
+gulp.task('zip_installer_files', function() {
+	gulp.src('recordings-installer/**', { base: '.' })
+		.pipe(zip(INSTALLER_ZIP_NAME))
 		.pipe(gulp.dest(OUTPUT_DIR));
 });
